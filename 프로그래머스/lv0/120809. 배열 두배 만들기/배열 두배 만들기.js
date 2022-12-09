a11cb3ab83b39f5b687e0aef1b8b836be9415fdc
Jihyun <75199356+wlgus3@users.noreply.github.com>
2022-12-10 00:49:02 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    var answer = [];
+    answer= numbers.map(function(el){return el*2})
+    // let answer = numbers.map(function(el){return el*2})
+    return answer;
+}
