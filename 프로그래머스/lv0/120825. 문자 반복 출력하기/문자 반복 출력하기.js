9504970f101d3fc2ca0e8fedf01aa3545242293c
Jihyun <75199356+wlgus3@users.noreply.github.com>
2022-12-17 00:05:30 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    for(i =0; i<my_string.length; i++){
+        for( j=0 ; j<n;j++){
+        answer= `${answer}${my_string[i]}`
+        }
+    }
+    return answer;
+}
