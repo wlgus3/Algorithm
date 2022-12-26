62ac3ba6595886dc8b01ccb2b81aff2d08f9f5f6
Jihyun <75199356+wlgus3@users.noreply.github.com>
2022-12-26 23:46:33 +0900

    [level 0] Title: 가위 바위 보, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,9 @@
+function solution(rsp) {
+    var answer = '';
+    for(i=0;i<rsp.length;i++){
+        if(rsp[i]==='0'){answer+=5}
+        if(rsp[i]==='2'){answer+=0}
+        if(rsp[i]==='5'){answer+=2}
+    }
+    return answer;
+}
