d7022a6d65663559b0cdcce9eb1e693d398a5261
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-01-16 21:30:59 +0900

    [level 0] Title: 모스부호 (1), Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,19 @@
+function solution(letter) {
+  var answer = '';
+  let letterArr= letter.split(' ')
+  const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+  for (i of letterArr){
+    answer+= morse[i]
+    
+  }
+
+
+  return answer;
+}
