18f2aaff684c97669d4de7d0a059b69982f5cab7
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-04-18 01:16:45 +0900

    [level 2] Title: 위장, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -0,0 +1,23 @@
+def solution(clothes):
+    answer = 1
+    #서로다른 옷의 조합 수 리턴, [의상이름,의상종류] 같은이름존재x 길이 20이하 알파벳소문자 혹은_로만 이루어짐 의상수는 1~30, 하루 최소 한 개의상입음 
+    dict={}
+    for i in clothes:
+        #if !dict[i[1]] :
+        if i[1] not in dict.keys() :
+            
+            dict[i[1]]=[]
+            dict[i[1]].append(i[0])
+
+            continue
+        else :
+            dict[i[1]].append(i[0])
+            continue
+    
+    print (dict)
+    keys=dict.keys()
+    for i in keys:
+        answer=answer*(len(dict[i])+1)
+        
+    
+    return answer-1
