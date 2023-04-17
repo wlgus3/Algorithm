7539756189b837e768b044cba69ec549010fd9e4
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-04-17 23:37:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.58 ms, Memory: 57 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,18 @@
+function solution(participant, completion) {
+    var answer = '';
+    //동명이인 있을 수 있단 게 포인트 
+    participant.sort()
+    completion.sort()
+    console.log(participant,completion)
+    for (let i=0;i<participant.length;i++){
+
+        if(participant[i]===completion[i]){
+            continue
+        }else{
+            answer=participant[i]
+            break
+        }
+    }
+    
+    return answer;
+}
