f1533fc237371f33c46c9f4a86be73ed95de9ec9
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-04-19 23:14:03 +0900

    [level 2] Title: 프린터, Time: 0.51 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -0,0 +1,27 @@
+def solution(priorities, location):
+    answer = 0
+    count=0
+    arr=[]
+    for i in range(len(priorities)): #로케이션 표시한 리스트 제작
+        if i == location:
+            arr.append(1)
+        else: arr.append(0)
+        
+    print(arr)
+    
+    while priorities != []:
+    # for i in priorities:
+        if priorities[0] == max(priorities):#인쇄한다면
+            count=count+1
+            if arr[0]==1: 
+                answer =count
+                break
+            priorities.pop(0)
+            arr.pop(0)
+        else: #인쇄 못함 맨 뒤로 
+            priorities.append(priorities[0])
+            priorities.pop(0)
+            arr.append(arr[0])
+            arr.pop(0)
+        print(priorities)
+    return answer
