2604e64fa11d859822f003f277e281904a1c74b0
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-04-21 23:56:50 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -8,7 +8,7 @@ def solution(numbers):
     # 1. 모든 수를 문자열로 변환
     numbers = list(map(str, numbers))
 
-    # 2. x+y와 y+x를 비교하여 정렬
+    # 2. 9가 90이나 9090보다 앞에 와야하기에 ->'9'*4='9999'로 만들어서 비교함 
     numbers.sort(key=lambda x: (x * 4)[:4], reverse=True)
     # 3. 정렬된 numbers를 이어붙인 뒤 반환
     answer = ''.join(numbers)
