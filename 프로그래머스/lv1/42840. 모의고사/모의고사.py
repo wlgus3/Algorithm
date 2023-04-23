92398e7fc7a56e933dcdccb7f8f19a8b87b30a49
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-04-23 23:54:31 +0900

    [level 1] Title: 모의고사, Time: 2.50 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,19 @@
+def solution(answers):
+    answer = [1]
+    answer1=[1,2,3,4,5]
+    answer2=[2, 1, 2, 3, 2, 4, 2, 5]
+    answer3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    correct=[0,0,0]
+    for i in range (len(answers)):
+        if answers[i]==answer1[i%5]:correct[0]+=1
+        if answers[i]==answer2[i%8]:correct[1]+=1
+        if answers[i]==answer3[i%10]:correct[2]+=1
+    print(correct)
+    maxCorrect=correct[0]
+    for i in range (1,len(correct)):
+        if correct[i] > maxCorrect:
+            answer=[i+1]
+            maxCorrect=correct[i]
+        elif correct[i] == maxCorrect:answer.append(i+1)
+        print(answer)
+    return answer
