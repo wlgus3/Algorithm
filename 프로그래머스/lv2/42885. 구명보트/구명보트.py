28bb5859bbe87754d5804c5cf2f92a6beaf81cda
Jihyun <75199356+wlgus3@users.noreply.github.com>
2023-04-28 23:43:16 +0900

    [level 2] Title: 구명보트, Time: 9.24 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,18 @@
+
+def solution(people, limit):
+    answer = 0
+
+    people = sorted(people)
+    l = 0
+    r = len(people)-1
+
+    while l <= r:
+        if people[l] + people[r] > limit:
+            answer += 1
+            r -= 1
+        else:
+            answer += 1
+            r -= 1
+            l += 1
+
+    return answer
