98efd714b675697414c42a6feb662c4b521e1181
Jihyun <jhjh3903@gmail.com>
2023-05-01 21:04:35 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.25 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,18 @@
+# def solution(n):
+#     if n==1:
+#         return 1
+#     elif n==2:
+#         return 2
+#     else :
+#         return (solution(n-1)+solution(n-2))%1234567
+
+def solution(n):
+    if n == 1:
+        return 1 % 1234567
+    elif n == 2:
+        return 2 % 1234567
+    else:
+        a,b = 1,2
+        for i in range(n-2):
+            a, b  = b, a+b
+        return b % 1234567
