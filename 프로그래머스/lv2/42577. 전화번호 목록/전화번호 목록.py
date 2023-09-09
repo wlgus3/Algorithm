2f8d625d2963a45fa99f93a37ea98fafefc4487b
Jihyun <jhjh3903@gmail.com>
2023-09-09 21:51:42 +0900

    [level 2] Title: 전화번호 목록, Time: 113.09 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,24 @@
-def solution(phone_book):
-    answer = True
-    #어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return
-    phone_book.sort()
-    for i in range(len(phone_book)-1)  :
-        if  phone_book[i]== phone_book[i+1][0:(len(phone_book[i]))]:
-            answer= False
-            break
-        else:
-            continue
+# def solution(phone_book):
+#     answer = True
+#     #어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return
+#     phone_book.sort()
+#     for i in range(len(phone_book)-1)  :
+#         if  phone_book[i]== phone_book[i+1][0:(len(phone_book[i]))]:
+#             answer= False
+#             break
+#         else:
+#             continue
     
-    return answer
+#     return answer
+
+def solution(phone_book):
+    answer=''
+    #문자열 기준으로 정렬 + 다음거랑 비교
+    phone_book.sort()
+
+    for i in range(len(phone_book)-1):
+        length= len(phone_book[i])
+        if len(phone_book[i])<=len(phone_book[i+1]):       
+            if phone_book[i+1][0:length]==phone_book[i]:
+                return False
+    return True
