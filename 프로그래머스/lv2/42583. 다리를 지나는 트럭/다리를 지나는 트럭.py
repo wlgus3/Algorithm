52db648f404a76c52f323908c0d7e0dd0cc9aa29
Jihyun <jhjh3903@gmail.com>
2023-09-10 12:48:56 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -29,11 +29,33 @@
 
 
 
+# def solution(bridge_length, weight, truck_weights):
+#     bridge=[]
+#     for i in range(bridge_length):
+#         bridge.append(0)
+#     # bridge=[0 for i in range(bridge_length)]
+#     count=0
+#     sumWeight=0
+#     while truck_weights!=[]:
+#         count+=1
+#         sumWeight-=bridge[-1]
+#         del bridge[-1]
+        
+#         bridge.insert(0,0)
+#         if sumWeight+truck_weights[0]<=weight:
+#             bridge[0]=truck_weights[0]
+#             sumWeight+=truck_weights[0]
+#             del truck_weights[0]
+
+#     return count+bridge_length
+
+
+
 def solution(bridge_length, weight, truck_weights):
-    # bridge=[]
-    # for i in range(bridge_length):
-    #     bridge.append(0)
-    bridge=[0 for i in range(bridge_length)]
+    bridge=[]
+    for i in range(bridge_length):
+        bridge.append(0)
+    # bridge=[0 for i in range(bridge_length)] #또다른 방법
     count=0
     sumWeight=0
     while truck_weights!=[]:
@@ -42,6 +64,7 @@ def solution(bridge_length, weight, truck_weights):
         del bridge[-1]
         
         bridge.insert(0,0)
+        #원래는 sum(bridge)로 다리위 무게총합을 구했는데 그렇게하니 시간초과 발생함 
         if sumWeight+truck_weights[0]<=weight:
             bridge[0]=truck_weights[0]
             sumWeight+=truck_weights[0]
