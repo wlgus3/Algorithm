860d6e10b39d348a6f37d762ddc3bcad4b162ef3
Jihyun <jhjh3903@gmail.com>
2023-09-14 23:56:05 +0900

    [level 1] Title: 소수 찾기, Time: 281.69 ms, Memory: 19.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+import math
+
+def solution(n):
+    answer = []
+    array = [True for i in range(n+1)]
+
+
+    for i in range(2, int(math.sqrt(n))+1):
+        if array[i] == True:
+            j = 2
+            while i * j <= n:
+                array[i*j] = False
+                j+=1
+
+    for i in range(2,n+1):
+        if array[i]:
+            answer.append(i)
+
+    return len(answer)
