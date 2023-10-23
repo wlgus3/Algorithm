b7966d3f46abfc0d8954385d836dd4610f13a9e3
Jihyun <jhjh3903@gmail.com>
2023-10-23 22:34:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.38 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,62 +1,36 @@
-#bfs를 쓰겠다
+    
+# bfs 함수 작성해서 bfs(0,0)의 값이 1이 나오면 -1을 리턴
+# 나머지의 경우는 bfs(0,0)의 값을 answer에 반환하는 형식
 from collections import deque
 def solution(maps):
-    max = len(maps) #maps 배열의 행
-    may = len(maps[0]) # maps 배열의 열
-    check = [[0]*may for i in range(max)]
-    # check에다가는 그냥 순서만 표시 (순서 == 방문여부)
-    # 방문 가능성 (=벽인지 아닌지)는 maps에서 처리
-    dy,dx = [-1,0,1,0],[0,1,0,-1]
-    q = deque([(0,0)])
-    check[0][0] = 1
+    answer = 0
+    n = len(maps) #높이
+    m = len(maps[0]) #너비
+    dx = [-1, 1, 0, 0]  #좌우상하 움직이기위한 방향표?
+    dy = [0, 0, -1, 1]
 
-    while q:
-        y,x = q.popleft()
-        for i in range(4):
-            ny,nx = y+dy[i] ,x+dx[i]
-            if 0<=ny<max and 0<=nx<may:
-                if check[ny][nx] == 0 and maps[ny][nx] == 1:
-                    check[ny][nx] = check[y][x] + 1
-                    q.append((ny,nx))
-    if check[max-1][may-1] == 0: # 방문하지 못했다면
+    def bfs(x,y):
+        q = deque() #큐는 bfs 구현시 검사 대기열
+        q.append((x,y)) #큐에 현재 좌표 추가하고 
+
+        while q:    
+            x, y = q.popleft()
+            for i in range(4):  #좌우 상하 갈 수 있는지 검사 
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:#맵을 벗어나면 무시
+                    continue
+                if maps[nx][ny] == 0:   #벽이면 무시
+                    continue
+                if maps[nx][ny] == 1:   #처음 지나가는 길이면 거리 계산하고 다시 상하좌우 확인 *만약 처음지나가지 않는다면 maps의 값이 1일 수가 없다.
+                    maps[nx][ny] = maps[x][y] + 1
+                    q.append((nx, ny))  #큐에 추가하면 사실상 재귀
+        return maps[n-1][m-1]   # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리 반환
+
+    if bfs(0,0) == 1:   #만약 도착지 숫자가1이라면 bfs이후에도 도착지 숫자가 변하지 않았기에 도착못한 것
         return -1
     else:
-        return check[max-1][may-1]
-    
-    
-    
-# #bfs 함수 작성해서 bfs(0,0)의 값이 1이 나오면 -1을 리턴
-# # 나머지의 경우는 bfs(0,0)의 값을 answer에 반환하는 형식으로 짰습니다.
-# from collections import deque
-# def solution(maps):
-#     answer = 0
-#     n = len(maps)
-#     m = len(maps[0])
-#     dx = [-1, 1, 0, 0]
-#     dy = [0, 0, -1, 1]
-
-#     def bfs(x,y):
-#         q = deque()
-#         q.append((x,y))
-
-#         while q:
-#             x, y = q.popleft()
-#             for i in range(4):
-#                 nx = x + dx[i]
-#                 ny = y + dy[i]
-
-#                 if nx < 0 or ny < 0 or nx >= n or ny >= m:
-#                     continue
-#                 if maps[nx][ny] == 0:
-#                     continue
-#                 if maps[nx][ny] == 1:
-#                     maps[nx][ny] = maps[x][y] + 1
-#                     q.append((nx, ny))
-#         return maps[n-1][m-1]
-
-#     if bfs(0,0) == 1:
-#         return -1
-#     else:
-#         answer = bfs(0,0)
+        answer = bfs(0,0)
 
     return answer
