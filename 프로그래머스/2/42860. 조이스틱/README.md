dd445475f56fa770286514bb917a1c821d9a7dec
Jihyun <jhjh3903@gmail.com>
2023-11-14 23:33:51 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.26 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 23:54:44
+2023년 11월 2일 23:33:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -31,52 +31,92 @@
 //     return answer;
 // }
 
+//정답 2
+// function solution(name) {
+//   let answer = 0;
+//   let min_move = name.length - 1;
 
+//   [...name].map((n, i) => {
+//     answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+//     let idx = i + 1;
+
+//     // 연속되는 A의 개수 count
+//     while (idx < name.length && name[idx] === 'A') {
+//       idx++;
+//     }
+
+//     min_move = Math.min(
+//       min_move,
+//       i * 2 + name.length - idx,
+//       i + 2 * (name.length - idx),
+//     );
+//   });
+
+//   return answer + min_move;
+// }
+
+//정답 2
 function solution(name) {
-  let answer = 0;
-  let min_move = name.length - 1;
-
-  [...name].map((n, i) => {
-    answer += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
-    let idx = i + 1;
-
-    // 연속되는 A의 개수 count
-    while (idx < name.length && name[idx] === 'A') {
-      idx++;
+    let sum = 0;
+    for (let i = 0; i < name.length; i++) {
+        let diff = name[i].charCodeAt() - 'A'.charCodeAt();
+        sum += diff > 13 ? 26 - diff : diff;
     }
 
-    min_move = Math.min(
-      min_move,
-      i * 2 + name.length - idx,
-      i + 2 * (name.length - idx),
-    );
-  });
+    let minMove = name.length - 1;
+    for (let i = 1; i < name.length; i++) {
+        if (name[i] === 'A') {
+            for (var j = i + 1; j < name.length; j++) {
+                if (name[j] !== 'A') {
+                    break;
+                }
+            }
 
-  return answer + min_move;
+            const left = i - 1;
+            const right = name.length - j;
+            minMove = Math.min(minMove, left > right ? left + right * 2 : left * 2 + right);
+
+            i = j;
+        }
+    }
+
+    return sum + minMove;
 }
-
 
 // function solution(name){
 //     var answer=0
 //     let alpha='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 //     alpha=alpha.split('')
-//     console.log(alpha)
+//     let reverseAlpha=[...JSON.parse(JSON.stringify(alpha)).reverse()]   //깊은복사 후 거꾸로
+//     console.log(alpha,reverseAlpha)
 //     let tmp=[]
 //     for ( i in name){
 //         let goal=alpha.indexOf(name[i])
-//         console.log(goal)
-//         tmp.push(goal)
+//         let goal2= reverseAlpha.indexOf(name[i])+1
+//         console.log(goal,goal2)
+//         tmp.push(Math.min(goal,goal2))
 //     }
 //     let count=0
 //     let pointer=0
 //     // tmp.findIdex() 00089. 00890 00009 08090 
-//     count+=Math.sum(tmp)
-//     print(count)
-//     // let normal=tmp.findIndex((el)=>el!=0)
-//     // let reversetmp=[...tmp.reverse()]
+//     count=tmp.reduce((x,y)=> x+y,0) //합 구하기
+//     let normal=tmp.findIndex((el)=>el!=0)
+//     let normalZero=tmp.findIndex((el)=>el==0)
+//     let reversetmp=[...JSON.parse(JSON.stringify(tmp)).reverse()]
+//     let reverse=reversetmp.findIndex((el)=>el!=0)
+//     let reverseZero=reversetmp.findIndex((el)=>el==0)
+//     console.log(tmp,normal,reverse,count)
+//     count+=tmp.length-1
+//     console.log(count)
+//     if (normal !=-1 && reversetmp!=-1){ //0이 있다고 나온다면
+//         maxminus=Math.max(normal,reverse)
+//         console.log(maxminus)
+//         count-=maxminus
+//     }
+    
 //     // let reverse=reversetmp.findIndex((el)=>el!=0)
 //     // if 
-    
-    
+//     console.log(count)
+//     answer=count
 //     return answer
 // }
