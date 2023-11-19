6f1884fb9e5eeeec4eadb06ce7e6ed835ad17014
Jihyun <jhjh3903@gmail.com>
2023-11-19 23:57:00 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 18.08 ms, Memory: 47.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -10,11 +10,14 @@
 //     var answer = -2;
 //     return answer;
 // }
+//기본적으로 합이 큰 큐에서 pop() 해서 작은 큐로 옯겨주는 방법
 function solution(q1, q2) {
+    //각 큐의 합 구해놓고
     let q1s = q1.reduce((acc, curr) => acc + curr, 0)
     let q2s = q2.reduce((acc, curr) => acc + curr, 0)
+    //최악의 경우 q1 값 전부 q2로 옮겼다가 다시 q2에 하나만 두고 q1으로 옮기는 경우 
     let limit = q1.length * 3
-    let dest = parseInt((q1s + q2s) / 2) // 목표값
+    let dest = (q1s + q2s) / 2 // 목표값
     let d1 = 0
     let d2 = 0
     let tried = 0
