325577f4fcc52042321b6b8e717b3fc90489cbc4
Jihyun <jhjh3903@gmail.com>
2023-11-27 12:52:40 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,12 +1,4 @@
 function solution(citations) {
-    //h번 이상 인용된 논문이 h편 이상이고 나머지논문이 h번 이하
-    // 결국 h는 최대값임 
     var answer = 0;
-    
-    citations.sort((a,b)=>b-a)//내림차순
-    for ( let i=0 ; i<citations.length; i++){
-        let casemax=Math.min(citations[i],i+1)
-        answer=answer<casemax?casemax:answer
-    }
     return answer;
 }
