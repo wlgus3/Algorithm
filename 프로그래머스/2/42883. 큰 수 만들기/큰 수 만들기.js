8be418d071f7d5978577a229e64bece8580a8e4e
Jihyun <jhjh3903@gmail.com>
2023-12-01 21:00:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,50 +1,68 @@
+// // 내가 못품
+// // https://school.programmers.co.kr/questions/45028
 
-//실패
+// //stack을 이용해서 풀자
 // function solution(number, k) {
-//     var answer = '';
-//     //안뽑아도 되는 n개 있다고 치면 number.length-n 만 뽑으면 됨
-//     //idx=0부터 순회하면서 ->시간복잡도 걸리겠는데 ...
-//     let n=number.length-k
-//     let max=0
-//     let count=0
-//     let nlength=number.length
-//     for ( i of number){
-//         if(nlength=n+count+i){ //6개중에 9:2 count:2 라면 4에서 멈춰야함
-//             for (j in range(count)){
-//                 answer+= String(max)
-                
-//             }
+//     let rest = k; //뺄 수 있는 글자수 -> 뺄 때마다 줄여갈 예정
+//     let newarr = number.split("");
+//     newarr.reverse(); //그냥 shift()하면 시간복잡도 손해
+//     let select = [];
+//     while (newarr.length > 0 && rest > 0) {
+//         //끝까지 순회했거나 더이상 뺄 수 없을때까지 반복
+//         select.push(newarr.pop()); //newarr의 끝값을 일단 select에 넣어놓고
+//         while (Number(select[select.length - 1]) < Number(newarr[newarr.length - 1]) && rest > 0) {
+//             //다음나올값과 비교해서 다음나올값보다 작은 수가 이미 select 됐다면 빼준다.
+//             rest -= 1;
+//             select.pop();
 //         }
-//         if(number[i]=='9'){
-//             answer+=number[i]
-//             n-=1
-//             max=0
-//             count=0
-//         }else if(Number(nubmer[i])>=max){
-//             max=Number(nubmer[i]
-//             if (Number(nubmer[i])==max){
-//                 count+=1
-//             }else{count=1}
+//     }
+//     if (rest !== 0) select = select.slice(0, -rest); //뺄 수 있는 게 남았다면 ==너무 많이 골랐으면 , 앞에서부터 잘라준다.
+
+//     return select.join("") + newarr.reverse().join(""); // 선택되지 못한 요소가 newarr에 있다면 붙여준다.
+// }
+
+
+
+// function solution(number, k) {
+//     //순회하면서 max를 갱신해야함
+//     var answer=''
+//     let maxNum=0
+//     let except=k
+//     for(let i=0;i<number.length;i++){
+//         let nowNum=Number(number[i])
+//         if(answer.length+except-i ==0){
+//             if(nowNum>=)
+//         }
+//         // 2 +          3+      +5 =10
+//         if (nowNum>maxNum){//선택한 최대값보다 크면 다 버리고 
+//             maxNum=nowNum
+//             except-=answer.length
+//             answer+=String(nowNum)
+//         }else if(nowNum==maxNum){
+            
 //         }
         
+//         //이전에 선택한 최대값과 같으면 선택,
+        
+        
+        
+        
 //     }
-//     return answer;
+//     return answer
 // }
 
 function solution(number, k) {
-    let stack = [];
+  const stack = [];
 
-    let arr = number.split('').reverse();
-
-    while(arr.length && k>0){
-        stack.push(arr.pop())
-        while(stack[stack.length-1] < arr[arr.length-1] && k>0) {
-            stack.pop();
-            k = k-1;
-        }
+  for (const num of number) {
+    while (k > 0 && stack[stack.length - 1] < num) {
+      stack.pop();
+      k--;
     }
+    stack.push(num);
+  }
 
-    if(k !== 0) stack = stack.slice(0, -k) 
+  stack.splice(stack.length - k, k);
 
-    return stack.join('') + arr.reverse().join('')
+  return stack.join("");
 }
