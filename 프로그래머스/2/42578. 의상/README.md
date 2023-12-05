31a952710d1bd326a4cc188c73bb337bf77d9e02
Jihyun <jhjh3903@gmail.com>
2023-12-05 20:50:34 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.23 ms
+메모리: 33.5 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 20:40:0
+2023년 12월 2일 20:50:32
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,18 @@
+//clotes=[['아이템',아이템 분류'],[...], ...]
+
 function solution(clothes) {
     let answer = 1;
     let obj={}
-    for ( i of clothes){
-        if (Object.keys(obj).includes(i[1])){
+    for ( i of clothes){    //clothes 순회하면서 1번인덱스 기준으로 object에 정리 
+        
+        if (obj.hasOwnProperty(i[1])){
             obj[i[1]].push(i[0])
         }else{
             obj[i[1]]=[i[0]]
         }
     }
-    for (j in obj){
-        answer=answer*(obj[j].length+1)
+    for (key in obj){ //key를 기준으로 아이템 착용 경우의 수대로 곱해준다.
+        answer=answer*(obj[key].length+1)
     }
-    return answer-1;
+    return answer-1;// 아무것도 착용하지 않은경우 제외 
 }
