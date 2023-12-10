a02d2f4c6306deeec00d8f3c4a250a7f4ee2defe
Jihyun <jhjh3903@gmail.com>
2023-12-10 21:16:18 +0900

    [level 3] Title: 베스트앨범, Time: 3.78 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 5.94 ms
+메모리: 33.7 MB, 시간: 3.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 21:2:34
+2023년 12월 0일 21:16:15
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -2,13 +2,7 @@ function solution(genres, plays) {
     let answer = [];
     let obj={}
     let sum=[]
-    // let sum ={}
     for( let i=0 ; i<genres.length;i++){
-        // if(genres[i] in sum){
-        //     sum[genres[i]]+=plays[i]
-        // }else{
-        //     sum[genres[i]]=plays[i]
-        // }
         for(let j=0;j<=sum.length;j++){
             if(j== sum.length){
                 sum.push([genres[i],plays[i]])
@@ -35,54 +29,8 @@ function solution(genres, plays) {
         }else{
             answer.push(obj[g[0]][0][1])
             answer.push(obj[g[0]][1][1])
-            // answer.push(obj[g[0]].pop()[1])
-            // answer.push(obj[g[0]].pop()[1])
         }
     }
     console.log(obj,sum)
     return answer;
 }
-
-// function solution(genres, plays) {
-//   const answer = [];
-
-//   // 노래를 구분하기 위한 배열을 만들어 준다.
-//   let songs = genres.map((genre, idx) => {
-//     return {
-//       no: idx,
-//       genre: genre,
-//       play: plays[idx],
-//     };
-//   });
-
-//   // 장르별 재생횟수를 위한 새로운 배열을 만들어 준다.
-//   let genrePlayCnt = [];
-//   songs.forEach((song) => {
-//     let thisGenre = genrePlayCnt.find((a) => a.genre === song.genre);
-//     if (thisGenre) {
-//       thisGenre.play += song.play;
-//     } else {
-//       genrePlayCnt.push({
-//         genre: song.genre,
-//         play: song.play,
-//       });
-//     }
-//   });
-  
-//   // 재생횟수가 많은 순으로 노래를 정렬한다.
-//   songs.sort((a, b) => b.play - a.play);
-//   // 재생횟수가 많은 순으로 장르를 정렬한다.
-//   genrePlayCnt.sort((a, b) => b.play - a.play);
-
-//   // 장르를 기준으로 배열을 돌면서 노래를 두 개 씩 넣는다.
-//   genrePlayCnt.forEach((a) => {
-//     let len = 0;
-//     songs.forEach((song) => {
-//       if (a.genre === song.genre && len < 2) {
-//         len++;
-//         answer.push(song.no);
-//       }
-//     });
-//   });
-//   return answer;
-// }
