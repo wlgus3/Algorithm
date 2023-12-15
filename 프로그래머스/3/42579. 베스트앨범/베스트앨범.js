17b67da38e076e83c1986c572b8468427bc5cbd3
Jihyun <jhjh3903@gmail.com>
2023-12-15 22:07:34 +0900

    [level 3] Title: 베스트앨범, Time: 4.72 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,36 +1,46 @@
 function solution(genres, plays) {
     let answer = [];
-    let obj={}
-    let sum=[]
-    for( let i=0 ; i<genres.length;i++){
-        for(let j=0;j<=sum.length;j++){
-            if(j== sum.length){
-                sum.push([genres[i],plays[i]])
+    let arrangedObj={}  //장르마다 정리할 object
+    let genresPlaysSum=[]  //장르별 재생수 합한 배열
+    for( let i=0 ; i<genres.length;i++){    
+        for(let j=0;j<=genresPlaysSum.length;j++){
+            if(j== genresPlaysSum.length){
+                genresPlaysSum.push([genres[i],plays[i]])
                 break
             }else{
-                if(sum[j][0]==genres[i]){
-                    sum[j][1]+=plays[i]
+                if(genresPlaysSum[j][0]==genres[i]){
+                    genresPlaysSum[j][1]+=plays[i]
                     break
                 }
             }
         }
-        if(genres[i] in obj){
-            obj[genres[i]].push([plays[i],i])
+        if(genres[i] in arrangedObj){
+            arrangedObj[genres[i]].push([plays[i],i])
         }else{
-            obj[genres[i]]=[[plays[i],i]]
+            arrangedObj[genres[i]]=[[plays[i],i]]
         }
-        obj[genres[i]].sort((a,b)=>b[0]-a[0])
+        arrangedObj[genres[i]].sort((a,b)=>b[0]-a[0])
     }
+    console.log(arrangedObj)
+    console.log(genresPlaysSum)
     
-    sum.sort((a,b)=>b[1]-a[1])
-    for (g of sum){
-        if(obj[g[0]].length==1){
-            answer.push(obj[g[0]][0][1])
+    genresPlaysSum.sort((a,b)=>b[1]-a[1])  //
+    // for (g of genresPlaysSum){
+    //     if(arrangedObj[g[0]].length==1){
+    //         answer.push(arrangedObj[g[0]][0][1])
+    //     }else{
+    //         answer.push(arrangedObj[g[0]][0][1])
+    //         answer.push(arrangedObj[g[0]][1][1])
+    //     }
+    // }
+    genresPlaysSum.forEach((el,idx)=>{
+        if(arrangedObj[el[0]].length==1){
+            answer.push(arrangedObj[el[0]][0][1])
         }else{
-            answer.push(obj[g[0]][0][1])
-            answer.push(obj[g[0]][1][1])
+            answer.push(arrangedObj[el[0]][0][1])
+            answer.push(arrangedObj[el[0]][1][1])
         }
-    }
-    console.log(obj,sum)
+    })
+    console.log(arrangedObj,genresPlaysSum)
     return answer;
 }
