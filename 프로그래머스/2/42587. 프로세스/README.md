6329aceea6e476f45d92a65234540badaa5d73db
Jihyun <jhjh3903@gmail.com>
2023-12-26 13:46:52 +0900

    [level 2] Title: 프로세스, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.22 ms
+메모리: 33.6 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 23:58:6
+2023년 12월 2일 13:46:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,52 +1,83 @@
-// //큐는 선입선출 
-// //priorities.length<=100 이기에 시간복잡도 자유로움
+//큐는 선입선출 
+//priorities.length<=100 이기에 시간복잡도 자유로움
 // function solution(priorities, location) {
 //     let answer = 0;
 //     let tmp=[...priorities]
-//     let queue=[]
+//     let queue=[]    //  [[중요도, 인덱스] ,[]...] 형태로 정리
 //     priorities.forEach((el,idx)=>{
 //         queue.push([el,idx])
 //     })
 //     // console.log(queue)
 //     let doneP=[]
-//     let maxP=Math.max(tmp)
-//     while(tmp.length>0){
-//         if(queue[0][0]==maxP){
-//             let now=queue.shift()
+//     let maxP=Math.max(...tmp)   //let maxP=Math.max(tmp) ->이렇게쓰면 NaN
+//     let count=0
+//     while(answer==0){
+//         let now=queue.shift()
+//         if(now[0]==maxP){
 //             doneP.push(now)
 //             if( now[1]==location){
-//                 answer=doneP.length()+1
+//                 answer=doneP.length
 //                 break
 //             }
-//             tmp.splice(now[1],1)
-//             maxP=Math.max(tmp)
-            
+//             // tmp.splice(now[1],1)
+//             maxP=Math.max(...tmp)
 //         }else{
-//             queue.push(queue.shift())
+//             queue.push(now)
+//         }
+//         // console.log(maxP,queue,doneP)
+//         count++
+//     }
+//     return answer;
+// }
+
+
+
+// function solution(priorities, location) {
+//     const copyPriorities = [...priorities]
+//     const dataset = priorities.map((priority,idx)=>({priority,idx}))
+//     let excuteCount = 0
+
+//     while(dataset.length !== 0){
+//         const maxValue = Math.max(...copyPriorities)
+//         const {priority,idx} = dataset.shift()
+//         if(priority >= maxValue){
+//             excuteCount++
+//             copyPriorities[idx] = 1
+//             if(idx === location){
+//                 break;
+//             }
+//         }
+//         else{
+//             dataset.push({priority,idx})
 //         }
 //     }
+//     return excuteCount
+// }
+
+
+function solution(priorities,location){
+    let answer=0
+    //priorities.length<=100이기에 시간복잡도 비교적 자유로움 
+    //[el,idx] 형태로 전부 정리
+    let queue=[]
+    priorities.forEach((el,idx)=>{
+        queue.push([el,idx])
+    })
+    // console.log(queue)
     
-//     return answer;
-// 
-
-function solution(priorities, location) {
-    const copyPriorities = [...priorities]
-    const dataset = priorities.map((priority,idx)=>({priority,idx}))
-    let excuteCount = 0
-
-    while(dataset.length !== 0){
-        const maxValue = Math.max(...copyPriorities)
-        const {priority,idx} = dataset.shift()
-        if(priority >= maxValue){
-            excuteCount++
-            copyPriorities[idx] = 1
-            if(idx === location){
-                break;
-            }
-        }
-        else{
-            dataset.push({priority,idx})
+    let sortedArr=priorities.sort((a,b)=>b-a)
+    let maxIdx=0
+    let done=[]
+    while(queue.length>0){
+        let front=queue.shift()
+        if(front[0]==sortedArr[maxIdx]){ //queue의 맨 앞 값이 최대 값과 같으면
+            done.push(front)
+            if(front[1]==location){break}
+            maxIdx++
+        }else{
+            queue.push(front)
         }
     }
-    return excuteCount
+    answer=done.length
+    return answer
 }
