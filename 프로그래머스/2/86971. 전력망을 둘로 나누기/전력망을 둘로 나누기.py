2ff83c96737bf5483f78e37bf28715053d8e0e37
Jihyun <jhjh3903@gmail.com>
2024-01-09 23:00:01 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 15.26 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -23,7 +23,8 @@
 #             minv = min(minv, abs(n-2*v))
 #     return minv
 
-# 0636~
+# 0636~0729 50m 인데 dfs구현 블로그 참고함ㅠ
+
 #전선중 하나를 끊어서 두 전력망으로 나누는데, 최대한 수가 비슷하게 나누려 하는데 두 송전탑 개수 차이 반환하라 
 
 # n= 송전탑수 max200 / wires=[[송전탑1,송전탑2],[],...] max n-1길이 
@@ -35,30 +36,19 @@
 from itertools import combinations
 from collections import deque
 import math
-def dfs(node,graph,visited,l):
-        q=deque()
-        q.append(node)
-        visited.append(node)
-        while q:
-            cur_node=q.popleft()
-            if cur_node in graph:
-                for next in graph[cur_node]:
-                    if next not in visited:
-                        l+=1
-                        visited.append(next)
-                        q.append(next)
-        return l
+
+
         # if node not in visited:
         #     for z in graph[node]:
         #         q.append(z)
         #         visited.append(z)
         
 def solution(n, wires):
-    combiWires=list(combinations(wires, len(wires)-1))
+    combiWires=list(combinations(wires, len(wires)-1))  #combiWires 에 연결 하나만 뺀 조합 경우의 수 모두 담는다. [(조합1), (조합2), ...] 형태가 될 예정
     # print(combiWires)
-    answer=n-2
+    answer=n-2  #가능한 최대값으로 초기세팅 
     for i in combiWires:
-        graph={}
+        graph={}    #각 노드의(네트워크의) 연결을 기록할 graph {1:[2], 2:[1,4],3:[7]..} 형태가 될 것 
         for connect in i:
             [node1,node2]=connect
             if node1 in graph:
@@ -71,24 +61,32 @@ def solution(n, wires):
                 graph[node2]=[node1]
         # print(graph)
         visited=[]
-        length=[]
+        length=[]   #그래프의 길이를 담아낼 배열 -> [n] or [n,n2] 형태로만 나올 것임 
         for n in graph:
-            if n not in visited:
-                length.append(dfs(n,graph,visited,1))
-        # print(length,visited)
-        if len(length)==2:
-            diff= ((length[0]-length[1])**2)**(1/2)
-            answer=min(answer,diff)
+            if n not in visited:    #방문하지 않은 노드만 dfs에 들어가도록 
+                length.append(dfs(n,graph,visited,1))   #
+        print(length)
+        if len(length)>=2:  #만약 길이가 1이라면 맨 끝쪽 노드만 잘라낸것 -> 네트워크를 두개로 나눈다는 조건에 부합x
+            # diff= ((length[0]-length[1])**2)**(1/2) 
+            diff= abs(length[0]-length[1])
             
-        
-        
-        
+            answer=min(answer,diff) #최소 차이값이 필요하니까
+               
     return answer
 
-
-
-
-
+def dfs(node,graph,visited,l):  #(현노드번호, 그래프기록, 방문기록, 그래프의길이누적)
+        q=deque()   
+        q.append(node)
+        visited.append(node)
+        while q:
+            cur_node=q.popleft()
+            if cur_node in graph:   #그래프에 있는지 확인해야함 내가 기록한 그래프는 연결없는 단독 노드라면 키가 존재하지 않음
+                for next in graph[cur_node]:
+                    if next not in visited:
+                        l+=1    #방문하면서 길이 누적 
+                        visited.append(next)    #방문여부 기록하고
+                        q.append(next)  #연결된 노드 탐색 대기열에 기록 
+        return l    #우리에게 필요한 것은 그래프의 길이이기에 길이 return
 
 
 
