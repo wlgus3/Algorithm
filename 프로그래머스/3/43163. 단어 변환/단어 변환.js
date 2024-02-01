b4db93adae5b7151a3c8856d466a1baf8945d152
Jihyun <jhjh3903@gmail.com>
2024-02-02 03:43:19 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,31 +1,67 @@
-function solution(begin, target, words) {
-  const visited = { [begin] : 0 };
-  const queue = [begin];
+// function solution(begin, target, words) {
+//     const visited = { [begin]: 0 };
+//     const queue = [begin];
 
-  while(queue.length) {
-    const cur = queue.shift();
-    
-    if(cur === target) break;
-    
-    for(const word of words) {
-      if(isConnected(word, cur) && !visited[word]) {
-        visited[word] = visited[cur] + 1;
-        queue.push(word);
-      }
+//     while (queue.length) {
+//         const cur = queue.shift();
+
+//         if (cur === target) break;
+
+//         for (const word of words) {
+//             if (isConnected(word, cur) && !visited[word]) {
+//                 visited[word] = visited[cur] + 1;
+//                 queue.push(word);
+//             }
+//         }
+//     }
+//     return visited[target] ? visited[target] : 0;
+// }
+
+// const isConnected = (str1, str2) => {
+//     let count = 0;
+//     const len = str1.length;
+
+//     for (let i = 0; i < len; i++) {
+//         if (str1[i] !== str2[i]) count++;
+//     }
+
+//     return count === 1 ? true : false;
+// };
+
+
+//
+function solution(begin, target, words) {
+    // const visited =Array(words.length).fill(false)
+    const visited = { [begin]: 0 };
+    const queue=[begin]
+    while(queue.length ){
+        const cur= queue.shift()
+        if (cur===target) break
+        for(let w of words){
+            if(isConnected(w, cur) && !visited[w]){
+                visited[w] =visited[cur]+1
+                queue.push(w)
+            }
+        }
     }
-  }
-  return visited[target] ? visited[target] : 0;
+    return visited[target]? visited[target]:0
+    
 }
 
 const isConnected = (str1, str2) => {
-  let count = 0;
-  const len = str1.length;
-  
-  for(let i = 0; i < len; i++) {
-    if(str1[i] !== str2[i]) count++;
-  }
-  
-  return count === 1 ? true : false;
-}
+    let count = 0;
+    const len = str1.length;
 
-//
+    for (let i = 0; i < len; i++) {
+        if (str1[i] !== str2[i]) count++;
+    }
+
+    return count === 1 ? true : false;
+};
+
+
+
+
+
+
+
