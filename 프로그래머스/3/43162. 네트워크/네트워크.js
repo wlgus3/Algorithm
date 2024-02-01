2db26e9f63eda02e06b927bfb8b3d49700022235
Jihyun <jhjh3903@gmail.com>
2024-02-02 02:32:36 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,42 +1,56 @@
-// function solution(n, computers) {
-//     let answer = 0;
-//     let graph={}
-//     for(let i=0 ;i<n; i++){
-//         graph[i]=computers[i]
-//     }
-//     console.log(graph)
-//     visited=Array(n).fill(false)
-//     console.log(visited)
-//     function dfs(now){
-//         return
-//     }
-    
-//     dfs(0)
-    
-//     return answer;
-// }
-
-
 function solution(n, computers) {
-    const visited = new Array(n).fill(0)
-    let count = 0;
-
-    for (let i=0; i<visited.length; i++) {
-        if (visited[i] === 1) continue;
-
-        count += 1;
-        dfs(computers, visited, i);
+    let answer = 0;
+    let graph={}
+    for(let i=0 ;i<n; i++){
+        graph[i]=computers[i]
     }
-
-    return count;
-}
-
-function dfs(map, visited, node) {
-    visited[node] = 1;
-
-    for (let i=0; i<map[node].length; i++) {
-        if ( map[node][i] && visited[i] === 0) {
-            dfs(map, visited, i);
+    // console.log(graph)
+    visited=Array(n).fill(false)
+    // console.log(visited)
+    for(let j=0 ;j<n;j++){
+        if(!visited[j]){
+            dfs(j)
+            // console.log(visited )
+            answer++
         }
     }
+    function dfs(now){
+        visited[now]=true
+        if(graph[now]){
+            for(let node=0;node<n;node++){
+                // console.log(node)//n:인덱스
+                if(graph[now][node]==1){    //연결되어있음 
+                    if(!visited[node]){dfs(node)}
+                }                
+            }
+        }
+        return
+    }
+    
+    return answer;
 }
+
+
+// function solution(n, computers) {
+//     const visited = new Array(n).fill(0)
+//     let count = 0;
+
+//     for (let i=0; i<visited.length; i++) {
+//         if (visited[i] === 1) continue;
+
+//         count += 1;
+//         dfs(computers, visited, i);
+//     }
+
+//     return count;
+// }
+
+// function dfs(map, visited, node) {
+//     visited[node] = 1;
+
+//     for (let i=0; i<map[node].length; i++) {
+//         if ( map[node][i] && visited[i] === 0) {
+//             dfs(map, visited, i);
+//         }
+//     }
+// }
