9e7594f4b8409caf5dd7f9b935669dae8d31726b
Jihyun <jhjh3903@gmail.com>
2024-04-08 22:17:10 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -4,7 +4,8 @@ function solution(clothes) {
     let answer = 1;
     let obj={}
     for ( i of clothes){    //clothes 순회하면서 1번인덱스 기준으로 object에 정리 
-        
+        // if (Object.keys(obj).includes(i[1])){    
+        // if (i[1] in obj){
         if (obj.hasOwnProperty(i[1])){
             obj[i[1]].push(i[0])
         }else{
@@ -16,3 +17,20 @@ function solution(clothes) {
     }
     return answer-1;// 아무것도 착용하지 않은경우 제외 
 }
+
+// function solution(clothes){
+//     let result=0
+//     //2차원 배열 clothes주어지고, 서로 다른 옷의 조합의 수를 return 
+//     let clothesObj={}
+//     // clothes.reduce((el)=>{
+//     //     clothesObj[el[1]]=clothesObj[el[1]]? [...clothesObj[el[1]],el[0]] : [el[0]]
+//     // })
+//     for (let i =0 ; i<clothes.length; i++){
+//         if (clothesObj[clothes[i][1]]){
+//             clothesObj[clothes[i][1]].append(clothes[i][0])
+//         }else { clothesObj[clothes[i][1]]=[clothes[i][0]]}
+//     }
+//     console.log(clothesObj)
+    
+//     return result
+// }
