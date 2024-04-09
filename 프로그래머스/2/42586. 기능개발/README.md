aa97dd09a500d74ff1f97ecd30d733ef93956d37
Jihyun <jhjh3903@gmail.com>
2024-04-09 23:58:36 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 4.77 ms
+메모리: 33.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 19:26:13
+2024년 04월 09일 23:58:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -4,25 +4,26 @@
 function solution(progresses, speeds) {
     let answer = [];
     let tmp = [...progresses]
-    let dayCal=[]
+    let dayCal=[]   // 각 기능이 몇일 뒤에 완료될지 정리할 배열
     for (let i=0; i<tmp.length;i++){
         let howMuchDays=Math.ceil((100-tmp[i])/speeds[i])
         dayCal.push(howMuchDays)
     }
-    console.log(dayCal)
+    // console.log(dayCal)
     
-    let pointer=0
+    let pointer=0  //배포기준이 되는 function 가리킴
+    //순회하면서 '현 기능'이 '배포기준 기능'보다 오래걸릴 예정인지 확인, 배포기준보다 오래걸리면 배포기준~ 직전 까지 한 번에 배포하고, 현 기능 이후를 새롭게 배포 
     dayCal.forEach((el,idx)=>{
-        if (idx==dayCal.length-1){
+        if (idx==dayCal.length-1){  //dayCal의 마지막 요소일때
             if(el>dayCal[pointer]){
                 answer.push(idx-pointer)
                 pointer=idx
             }
-            answer.push(idx-pointer+1)
+            answer.push(idx-pointer+1)  //배포기준~ 현재까지 배포 
         }
-        else if (el>dayCal[pointer]){    //최대보다 오래걸리면?
-            answer.push(idx-pointer)
-            pointer=idx
+        else if (el>dayCal[pointer]){    //현F가 배포기준F보다 오래걸림
+            answer.push(idx-pointer)    //직전 F까지 배포 
+            pointer=idx     //배포기준 F를 현F로 바꿈
         }
     })
     return answer;
