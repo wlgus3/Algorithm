360e5b5817a8db4d539ae7a7fdd648b9bf5cbad0
Jihyun <jhjh3903@gmail.com>
2024-04-10 23:57:11 +0900

    [level 2] Title: 타겟 넘버, Time: 1.38 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 1.13 ms
+메모리: 35.6 MB, 시간: 1.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 1:54:22
+2024년 04월 10일 23:57:09
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,33 @@
-function solution(numbers, target) {
-    var answer = 0;
-    //재귀를 만들어서 인덱스와, 합계를 함께 넘기고 인덱스가 끝까지 다다르면 값 비교후 answer+
-    const  recursion= (index, sum)=>{
-        if (index== numbers.length ){
-            if (sum==target) {answer+=1}
+// function solution(numbers, target) {
+//     var answer = 0;
+//     //재귀를 만들어서 인덱스와, 합계를 함께 넘기고 인덱스가 끝까지 다다르면 값 비교후 answer+
+//     const  recursion= (index, sum)=>{
+//         if (index== numbers.length ){
+//             if (sum==target) {answer+=1}
 
-            return
-        }
-        //더하거나 빼는 두가지 경우의 수
-        recursion(index+1, sum+ numbers[index])
-        recursion(index+1, sum- numbers[index])
+//             return
+//         }
+//         //더하거나 빼는 두가지 경우의 수
+//         recursion(index+1, sum+ numbers[index])
+//         recursion(index+1, sum- numbers[index])
         
+//     }
+//     recursion(0,0)
+//     return answer;
+// }
+
+function solution(numbers,target){
+    let answer=0
+    function recursion(idx, num){
+        if( idx==numbers.length ){
+            if (num ==target){
+                answer+=1
+            }
+            return 
+        }
+        recursion(idx+1, num +numbers[idx])
+        recursion(idx+1, num -numbers[idx])
     }
     recursion(0,0)
-    return answer;
+    return answer
 }
