011d7e87f7fefb6567c9337806553e84dcc84f02
Jihyun <jhjh3903@gmail.com>
2024-07-08 23:32:25 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -30,33 +30,6 @@
 //     return answer;
 // }
 
-
-// function solution(n, computers) {
-//     const visited = new Array(n).fill(0)
-//     let count = 0;
-
-//     for (let i=0; i<visited.length; i++) {
-//         if (visited[i] === 1) continue;
-
-//         count += 1;
-//         dfs(computers, visited, i);
-//     }
-
-//     return count;
-// }
-
-// function dfs(map, visited, node) {
-//     visited[node] = 1;
-
-//     for (let i=0; i<map[node].length; i++) {
-//         if ( map[node][i] && visited[i] === 0) {
-//             dfs(map, visited, i);
-//         }
-//     }
-// }
-
-
-//24/04/11
 function solution(n, computers){
     let answer=0
     const check =Array(n).fill(false)
